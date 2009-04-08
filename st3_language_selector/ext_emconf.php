688ca7f9f977fd52c92f771a501fd2729fb0afe2
rxergio <rxergio@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-08 14:01:58 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st3_language_selector/trunk@18836 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ st3_language_selector/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "st3_lang"
+#
+# Auto generated 06-04-2009 17:46
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Language selector',
+	'description' => 'Plugin to select the language.',
+	'category' => 'plugin',
+	'author' => 'Sergio San Juan',
+	'author_email' => 'ssanjuan@st3elkartea.net',
+	'shy' => '',
+	'dependencies' => 'sr_static_info',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'ST3elkartea',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"5883";s:14:"ext_emconf.php";s:4:"beba";s:17:"ext_localconf.php";s:4:"6854";s:28:"ext_typoscript_constants.txt";s:4:"afc8";s:24:"ext_typoscript_setup.txt";s:4:"9f67";s:28:"pi1/class.tx_st3lang_pi1.php";s:4:"f664";s:10:"README.txt";s:4:"ee2d";s:22:"templates/default.tmpl";s:4:"ff1f";s:12:"ext_icon.gif";s:4:"1fcd";}',
+);
+
+?>
